02edc6efb5f8301c08467ccced5478788676ec74
Kadir Can Gönüllü <gonullukadircan@gmail.com>
2026-08-13 21:24:04 +0300

    v0.7.5

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+document.addEventListener("keydown", (e) => {
+  if (e.key === "F12") {
+    e.preventDefault();
+  }
+
+  if (e.ctrlKey && e.shiftKey && e.key === "I") {
+    e.preventDefault();
+  }
+});
+
+document.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+});
+
 const hero = document.querySelector(".hero");
 const sleeve = document.getElementById("recordSleeve");
 const platter = document.getElementById("platter");
